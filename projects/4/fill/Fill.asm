cbafdab4429be82abb80fb82b32ac39ed0d6dcaf
Dan Tu <23713851+dan-tu@users.noreply.github.com>
2026-08-13 21:27:44 -0700

    Implement project 4

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,53 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+// SCREEN = 256 rows x 512 pixels (32 x 16-bit words)  @ 0x4000
+// KBD = 16 bits @ 0x6000
+// 0 = white, 1 = black
+
+(LOOP)
+    @SCREEN
+    D=A
+    @R0
+    M=D
+
+    @KBD
+    D=M
+    @FILLWHITE
+    D;JEQ
+    @FILLBLACK
+    0;JMP
+
+(FILLWHITE)
+    @R1
+    M=0
+    @FILLSCREEN
+    0;JMP
+
+(FILLBLACK)
+    @R1
+    M=-1
+    @FILLSCREEN
+    0;JMP
+
+(FILLSCREEN)
+    // Fill the pixel with the color from R1
+    @R1
+    D=M
+    @R0
+    A=M
+    M=D
+
+    // Check if all pixels have been processed
+    @R0
+    D=M+1
+    @KBD
+    D=A-D
+    @LOOP
+    D;JEQ
+
+    // Otherwise, keep filling pixels
+    @R0
+    M=M+1
+    @FILLSCREEN
+    0;JMP
